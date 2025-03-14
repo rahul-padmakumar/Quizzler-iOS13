569f2b30f43a3c3a99534e40608613d19acd147a
rahulp <rahulp@qburst.com>
2025-03-14 10:53:28 +0530

    Navigating through questions and comparing answer using 2D array

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    let questionAnswerArray = [
+        ["Four + two is six", "True"],
+        ["Four is less than 3", "False"],
+        ["2 * 2 is 4", "True"]
+    ]
+    
+    var questionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    
+    @IBAction func onAnswerPressed(_ sender:UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let wrightAnswer = questionAnswerArray[questionIndex][1]
+        
+        if userAnswer == wrightAnswer{
+            print("Wright Answer")
+        } else {
+            print("Wrong answer")
+        }
+        
+        proceedToNextQuestion()
+    }
+    
+    func proceedToNextQuestion(){
+        questionIndex = (questionIndex + 1) % questionAnswerArray.count
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = questionAnswerArray[questionIndex][0]
     }
 
 
